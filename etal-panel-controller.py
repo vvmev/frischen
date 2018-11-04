3fb6c95a805e8ffd009b43bcb6df78ab24880336
Stefan Bethke <stb@lassitu.de>
2018-11-04 22:06:06 +0100

    Allow switching shunting signals

--- etal-panel-controller.py
+++ etal-panel-controller.py
@@ -4,13 +4,25 @@
 import logging
 import asyncio
 
-from frischen.panelcontroller import Switch, SwitchController
+from frischen.panelcontroller import Signal, Switch, SwitchController
 
 logger = logging.getLogger(__name__)
 
 
 async def main():
     controller = SwitchController('frischen/etal/panel')
+    signals = {}
+    signals['p1p3'] = Signal(controller, 'p1p3').distant()
+    signals['A'] = Signal(controller, 'A').home().alt()
+    signals['a'] = Signal(controller, 'a').distant()
+    signals['P1'] = Signal(controller, 'P1').home().shunting().alt()
+    signals['P3'] = Signal(controller, 'P3').home().shunting().alt()
+    signals['N2'] = Signal(controller, 'N2').home().shunting().alt()
+    signals['N3'] = Signal(controller, 'N3').home().shunting().alt()
+    signals['F'] = Signal(controller, 'F').home().alt().distant()
+    signals['f'] = Signal(controller, 'f').distant()
+    signals['n2n3'] = Signal(controller, 'n2n3').distant()
+    controller.signals = signals
     switches = {k: Switch(controller, k)
                 for k in ['W1', 'W2', 'W3', 'W4', 'W10', 'W11', 'W12', 'W13']}
     controller.switches = switches
--- frischen/panelcontroller.py
+++ frischen/panelcontroller.py
@@ -8,6 +8,51 @@ from hbmqtt.mqtt.constants import QOS_0
 logger = logging.getLogger(__name__)
 
 
+class Signal():
+    def __init__(self, controller, name):
+        self.controller = controller
+        self.name = name
+        self.aspect = 'Hp0'
+        self.aspects = []
+
+    def __repr__(self):
+        return f'Signal<{self.name}>'
+
+    def alt(self):
+        self.aspects += ['Zs1']
+        return self
+
+    def distant(self):
+        self.aspects += ['Vr0', 'Vr1', 'Vr2']
+        return self
+
+    def home(self):
+        self.aspects += ['Hp0', 'Hp1', 'Hp2']
+        return self
+
+    def shunting(self):
+        self.aspects += ['Sh1']
+        return self
+
+    def publish(self):
+        self.controller.publish(
+            self.controller.topic('signal', self.name),
+            self.aspect.encode('utf-8')
+        )
+
+    def set(self, aspect):
+        self.aspect = aspect
+        self.publish()
+
+    async def change_shunting(self):
+        if 'Sh1' in self.aspects:
+            if self.aspect == 'Hp0':
+                self.aspect = 'Sh1'
+            else:
+                self.aspect = 'Hp0'
+            self.publish()
+
+
 class Switch():
     def __init__(self, controller, name):
         self.controller = controller
@@ -48,6 +93,10 @@ class SwitchController():
         self.switch_group_button_state = 0
         self.switch_group_button_reset = None
         self.switches = {}
+        self.signal_group_button = 'SGT'
+        self.signal_group_button_state = 0
+        self.signal_group_button_reset = None
+        self.signals = {}
         self.base_topic = base_topic
         logger.setLevel(logging.DEBUG)
 
@@ -73,22 +122,41 @@ class SwitchController():
         logger.debug(f'Started signal_group_button timeout')
         await asyncio.sleep(5)
         logger.debug(f'signal_group_button timeout reached')
+        self.signal_group_button_state = 0
+        self.publish(
+            self.topic('button', self.signal_group_button), b'0')
+        self.signal_group_button_reset = None
+
+    def start_reset_signal_group_button(self):
+        if self.signal_group_button_reset:
+            self.signal_group_button_reset.cancel()
+        self.signal_group_button_reset = asyncio.create_task(
+            self.reset_signal_group_button())
+
+    async def reset_switch_group_button(self):
+        logger.debug(f'Started switch_group_button timeout')
+        await asyncio.sleep(5)
+        logger.debug(f'switch_group_button timeout reached')
         self.switch_group_button_state = 0
         self.publish(
             self.topic('button', self.switch_group_button), b'0')
         self.switch_group_button_reset = None
 
-    def start_reset_signal_group_button(self):
+    def start_reset_switch_group_button(self):
         if self.switch_group_button_reset:
             self.switch_group_button_reset.cancel()
         self.switch_group_button_reset = asyncio.create_task(
-            self.reset_signal_group_button())
+            self.reset_switch_group_button())
 
     async def handle(self):
         self.publish(
             self.topic('button', self.switch_group_button), b'0')
         for s in self.switches.values():
             s.set(0, 0)
+        self.publish(
+            self.topic('button', self.signal_group_button), b'0')
+        for s in self.signals.values():
+            s.set('Hp0')
         await self.client.subscribe([
             (self.topic('button', '#'), QOS_0)])
         try:
@@ -100,15 +168,30 @@ class SwitchController():
 
                 button = self.subject(topic, 'button')
                 value = int(value)
+
+                if button == self.signal_group_button:
+                    self.signal_group_button_state = value
+                    if value == 1:
+                        self.start_reset_signal_group_button()
+
                 if button == self.switch_group_button:
                     self.switch_group_button_state = value
                     if value == 1:
-                        self.start_reset_signal_group_button()
+                        self.start_reset_switch_group_button()
+
                 if button in self.switches:
                     if self.switch_group_button_state == 1 and \
                             value == 1:
                         self.start_reset_signal_group_button()
-                        asyncio.ensure_future(self.switches[button].change())
+                        asyncio.create_task(self.switches[button].change())
+
+                if button in self.signals:
+                    if self.signal_group_button_state == 1 and \
+                            value == 1:
+                        self.start_reset_signal_group_button()
+                        asyncio.create_task(
+                            self.signals[button].change_shunting())
+
             await self.client.unsubscribe(['frischen/time/#'])
             await self.client.disconnect()
         except ClientException as ce:
